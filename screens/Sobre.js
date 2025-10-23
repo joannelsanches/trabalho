2466aecc00fbe43dbb983532fcb6c6b7b7fffda3
joh <joannesanches.bg013@academico.ifsul.edu.br>
2025-10-23 17:19:22 -0300

    Update Sobre.js

--- screens/Sobre.js
+++ screens/Sobre.js
@@ -1,13 +1,15 @@
 import { View, Text } from 'react-native';
 import styles from '../styles';
 
-export default function Sobre(){
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.titulo}>Sobre o Aplicativo</Text>
-            <Text>Criado na aula da Desenvolvimento para Dispositivos Móveis</Text>
-        </View>
-    );
-
+export default function Sobre() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Sobre o Aplicativo</Text>
+      <Text style={styles.texto}>
+        O aplicativo *Alphínia TOUR* foi desenvolvido para que os hóspedes
+        do Alphínia Palace Hotel possam explorar os principais pontos turísticos
+        da Escócia com facilidade.
+      </Text>
+    </View>
+  );
 }
