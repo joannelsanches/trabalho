2d3be338b6d946c7c2a61c41b70a9d99fac983f7
joh <joannesanches.bg013@academico.ifsul.edu.br>
2025-10-23 16:39:24 -0300

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,25 +1,23 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import styles from '../styles';
 import { useNavigation } from '@react-navigation/native';
 import { TextInput } from 'react-native-gesture-handler';
 
-export default function Login(){
-    const navigation = useNavigation();
+export default function Login() {
+  const navigation = useNavigation();
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.titulo}>LOGIN</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>LOGIN</Text>
+      <TextInput placeholder="Email" style={styles.input} />
+      <TextInput placeholder="Senha" style={styles.input} secureTextEntry />
 
-            <TextInput placeholder='email' style={styles.input}/>
-            <TextInput placeholder='senha' style={styles.input} secureTextEntry={true} />
-
-            <TouchableOpacity 
-                style={styles.botao}
-                onPress={ () => navigation.navigate('Página Inicial')}
-            >
-                <Text style={styles.botaoTexto}>LOGIN</Text>
-            </TouchableOpacity>
-        </View>
-    );
-
+      <TouchableOpacity 
+        style={styles.botao}
+        onPress={() => navigation.navigate('Página Inicial')}
+      >
+        <Text style={styles.botaoTexto}>ENTRAR</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
